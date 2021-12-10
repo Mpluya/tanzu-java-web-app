f7e71206b2ef205eca9f8723654f1b0244897124
Mae Large <mlarge@vmware.com>
2021-12-10 13:22:30 -0700

    testing supply chain

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Unicorns and butterflies";
+		return "Greetings from dancing unicorns";
 	}
 
 }
