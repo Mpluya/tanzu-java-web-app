3d95e1a5aec10546824410b488d587e27dd3c30e
Mae Large <mlarge@vmware.com>
2022-06-22 14:42:04 -0500

    nudge

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "GREENER Tanzu";
+		return "GREENEST Tanzu";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("GREENER Tanzu", controller.index());
+        assertEquals("GREENEST Tanzu", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("GREENER Tanzu"));
+            .andExpect(content().string("GREENEST Tanzu"));
     }
 }
